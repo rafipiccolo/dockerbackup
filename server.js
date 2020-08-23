42d92e5a093dec1ce83f3ae99bb481be8df5bcc2
root <rafi.piccolo@gmail.com>
2020-08-23 15:10:57 +0200

    x

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const port = process.env.PORT||3000;
 const influxdb = require('./lib/influxdb');
 
 app.get('/', async (req, res, next) => {
+    res.sendFile(__dirname+'/index.html')
+});
+
+app.get('/data', async (req, res, next) => {
     try {
         var sql = '';
         if (req.query.error)
-            sql = `select * from dockerbackup where "error" = 1 and "backuphost" = '${process.env.HOSTNAME}' order by time desc limit 100`;
+            sql = `select * from dockerbackup where "error" = 1 and "backuphost" = '${process.env.HOSTNAME}' order by time desc limit 1000`;
         else
-            sql = `select * from dockerbackup where "backuphost" = '${process.env.HOSTNAME}' order by time desc limit 100`;
+            sql = `select * from dockerbackup where "backuphost" = '${process.env.HOSTNAME}' order by time desc limit 1000`;
         var data = await influxdb.query(sql);
         res.send(data);
     } catch(err) {
