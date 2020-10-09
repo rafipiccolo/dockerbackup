ee3e382445fece8e2de3dde5067449fb1486b7c4
root <rafi.piccolo@gmail.com>
2020-10-09 12:16:04 +0200

    ameliorations front

--- clean.js
+++ clean.js
@@ -1,7 +1,18 @@
 const moment = require('moment');
 const path = require('path');
 const glob = require('util').promisify(require('glob'));
-const rimraf = require('util').promisify(require('rimraf'));
+var execFile = require('child_process').execFile;
+
+function execFilePromise(cmd, params) {
+    return new Promise((resolve, reject) => {
+        execFile(cmd, params, function (err, stdout, stderr) {
+            if (err) return reject(err);
+            if (stderr) return reject(stderr);
+
+            resolve(stdout);
+        });
+    })
+}
 
 (async () => {
     var dirs1 = await glob('/backup/*/all/*');
@@ -10,13 +21,13 @@ const rimraf = require('util').promisify(require('rimraf'));
 
     for (var dir of dirs) {
         var basename = path.basename(dir);
-        var m = basename.match(/(\d+-\d+-\d+)/);
+        var m = basename.match(/^(\d+-\d+-\d+)/);
         if (!m) continue;
 
         var date = new Date(m[1]);
         if (date < moment().add(-30, 'd')) {
             console.log('deleting', dir);
-            await rimraf(dir, {glob: false});
+            await execFilePromise('rm', ['-rf', dir]);
         }
     }
 })()
