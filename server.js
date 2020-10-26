2b92708f119efffab7d8112704f5b3cc257d113e
root <rafi.piccolo@gmail.com>
2020-10-26 17:46:27 +0100

    x

--- server.js
+++ server.js
@@ -41,50 +41,6 @@ app.get('/data', async (req, res, next) => {
     }
 })
 
-app.get('/cron/alert', async (req, res, next) => {
-    try {
-        var errors = await influxdb.query(`select * from dockerbackup where "backuphost" = '${process.env.HOSTNAME}' and error = 1 and time > now() - 1d order by time desc limit 1000`);
-        var rsyncs = await influxdb.query(`select * from dockerbackup where "backuphost" = '${process.env.HOSTNAME}' and driver = 'rsync' and time > now() - 1d order by time desc limit 1000`);
-        var df = await checkDf({ path: '/' });
-
-        var html = '';
-        var maxpercent = 90;
-        if (df.percent > maxpercent) {
-            html += `${process.env.HOSTNAME} Used disk space > ${maxpercent}\n`
-        }
-        
-        html += `\n`
-        if (errors.length) {
-            html += 'Errors :\n'
-            html += errors.map(error => {
-                var url = `https://monitoring.raphaelpiccolo.com/logsMysql?containerName=/dockerbackup&hostname=${process.env.HOSTNAME}&start=${moment(error.time).add(-1, 'm').toDate().getTime()}000000&end=${moment(error.time).add(+1, 'm').toDate().getTime()}000000`;
-                return `<a href="${url}">${moment(error.time).format('YYYY-MM-DD HH:mm:ss')}</a> ${error.driver} ${error.host}\n`;
-            }).join('');
-        }
-        
-        html += `\n`
-        rsyncs = rsyncs.filter(rsync => (rsync.sizeTransfert / rsync.size * 100) > 5);
-        rsyncs = rsyncs.filter(rsync => (rsync.sizeTransfert > 100000));
-        if (rsyncs.length) {
-            html += 'RSYNC too much :\n'
-            html += rsyncs.map(rsync => `${moment(rsync.time).format('YYYY-MM-DD HH:mm:ss')} ${rsync.host} ${(rsync.sizeTransfert / rsync.size) * 100}%\n`).join('');
-        }
-
-        if (html.trim() == '') return res.send('nothing to send');
-
-        await sendMail({
-            to: "rafi.piccolo@gmail.com, martin.wb.2015@gmail.com",
-            subject: "dockerbackup " + process.env.HOSTNAME,
-            text: html,
-            html: html.replace(/\n/g, '<br />'),
-        });
-
-        res.send('ok');
-    } catch (err) {
-        next(err);
-    }
-})
-
 app.get('/health', (req, res) => {
     res.send('ok')
 })
